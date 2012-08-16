eb5a4b0951f253f42f6eeb3f3a632354dca2b50b
Diego Netto <diegormnetto@gmail.com>
2012-08-16 18:58:32 -0400

    Designed and implemented inline-editing for the UI of the DebtView.

--- public/javascripts/views/debt.js
+++ public/javascripts/views/debt.js
@@ -17,7 +17,13 @@ $(function() {
 
 		// The DOM events specific to a debt.
 		events: {
-			'click .destroy-btn':	'clear'
+			'click .destroy-btn':		'clear',
+			'dblclick .title-cell':		'editTitle',
+			'dblclick .type-cell':		'editType',
+			'dblclick .principal-cell':	'editPrincipal',
+			'dblclick .rate-cell':		'editRate',
+			'dblclick ..repayment-cell':	'editRepayment',
+			'blur .edit':			'close'
 		},
 
 		// The DebtView listens for changes to its model, re-rendering. Since there's
@@ -39,7 +45,64 @@ $(function() {
 		// its view (since the OrganizeView is listening for changes in the collection).
 		clear: function() {
 			this.model.destroy();
-		}
+		},
 
+		// Set editing mode on and display the input field for debt Title.
+		// Position the input within the cell.
+		editTitle: function( event ) {
+			var cell = this.$('.title-cell');
+			var element = this.$('.edit-title');
+			this.overlayInCell( cell, element );
+			cell.addClass('editing');
+			element.focus();
+		},
+
+		// Set editing mode on and display the input field for debt Type.
+		editType: function( event ) {
+			var cell = this.$('.type-cell');
+			var element = this.$('.edit-type');
+			element.width( cell.outerWidth() );
+			cell.addClass('editing');
+			element.focus();
+		},
+
+		// Set editing mode on and display the input field for debt Principal.
+		editPrincipal: function( event ) {
+			var cell = this.$('.principal-cell');
+			var element = this.$('.edit-principal');
+			this.overlayInCell( cell, element );
+			cell.addClass('editing');
+			element.focus();
+		},
+
+		// Set editing mode on and display the input field for debt Rate.
+		editRate: function( event ) {
+			var cell = this.$('.rate-cell');
+			var element = this.$('.edit-rate');
+			this.overlayInCell( cell, element );
+			cell.addClass('editing');
+			element.focus();
+		},
+
+		// Set editing mode on and display the input field for debt Repayment.
+		editRepayment: function( event ) {
+			var cell = this.$('.repayment-cell');
+			var element = this.$('.edit-repayment');
+			this.overlayInCell( cell, element );
+			cell.addClass('editing');
+			element.focus();
+		},
+
+		// TODO:
+		close: function() {
+			this.$('.editing').removeClass('editing');
+		},
+
+		// Overlays an element complete within its parent table cell.
+		overlayInCell: function( cell, element ) {
+			element.width( cell.outerWidth() );
+			// Subtract 1px to account for bottom border
+			element.height( cell.outerHeight() - 1);
+		}
 	});
 });
