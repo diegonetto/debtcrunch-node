7de97f2c3e9e620623fbccb42651dd91a1badcd9
Diego Netto <diegormnetto@gmail.com>
2012-08-23 20:34:06 -0400

    Fixed bug causing debt principal and rate to be saved as strings.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,7 +7,7 @@ var DEBT_TYPES = ['Credit Card', 'Stafford Loan', 'Perkins Loan', 'Plus Loan'];
 
 // Round a float only if it is not NaN.
 app.toFixed = function( value ) {
-	return value ? accounting.toFixed( value, 2 ) : value;
+	return value ? parseFloat( accounting.toFixed(value, 2) ): value;
 }
 
 $(function() {
