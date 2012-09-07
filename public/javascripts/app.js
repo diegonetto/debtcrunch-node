249db1510ff2d677abdeb9ad01af140c3bd69e04
Diego Netto <diegormnetto@gmail.com>
2012-09-07 04:20:36 -0400

    Added TAB_KEY code to app variables.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,6 @@
 var app = app || {};
 var ENTER_KEY = 13;
+var TAB_KEY = 9;
 var DEBT_TYPES = ['Credit Card', 'Stafford Loan', 'Perkins Loan', 'Plus Loan', 'Mortgage', 'Other'];
 var DEBT_FIELDS = ['title', 'type', 'principal', 'rate', 'repayment'];
 var STAFFORD_LOAN_MIN = 50.0;
