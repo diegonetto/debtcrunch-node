fdf41a2b44ddbd626f578cbbe5bdbbcbf1d75ce4
Diego Netto <diegormnetto@gmail.com>
2012-09-09 00:31:28 -0400

    Moved serving of static assets to only development mode. Closed off routes to home and faq pages, with root pointing to /app.

--- debtcrun.ch.js
+++ debtcrun.ch.js
@@ -13,16 +13,16 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+//  app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 /**
@@ -55,9 +55,10 @@ app.use(function(req, res, next){
 /**
  * Routing
  */
-app.get('/', routes.home);
+//app.get('/', routes.home);
+app.get('/', routes.app);
 app.get('/app', routes.app);
-app.get('/faq', routes.faq);
+//app.get('/faq', routes.faq);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
